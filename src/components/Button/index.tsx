b6e5df50f2342bf8ffb1a45779409b417fb882a1
AhmadJanuarr <ahmadjanuaramri@gmail.com>
2024-10-09 21:13:13 +0700

    perf : create reusable buttons components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,20 @@
-export default function ButtonCustom({ children }: {
-    children: React.ReactNode
+import Image from "next/image";
+
+export default function ButtonCustom({
+    children,
+    src,
+    className,
+}: {
+    src: string;
+    className?: string;
+    children: React.ReactNode;
 }) {
     return (
-        <button className="px-3 py-3 text-white bg-[#375ACB] rounded-[10px] text-[20px] font-neuBold">
-            {children}
+        <button
+            className={`flex items-center gap-2 px-3 py-2 text-white bg-secondary rounded-[10px] md:text-[20px] font-neuBold justify-center border border-[#132C92] ${className}`}
+        >
+            <Image src={src} alt="user" width={24} height={24} />
+            <span>{children}</span>
         </button>
-    )
+    );
 }
