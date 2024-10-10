8c4124753042644064b8f76558601d495144b972
AhmadJanuarr <ahmadjanuaramri@gmail.com>
2024-10-11 01:10:51 +0700

    refactor : separate a component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -5,7 +5,7 @@ export default function SosialMediaIcon() {
     return (
         <div className="flex space-x-5">
             {ICON_LIST.map(({ id, link, src, name }) => (
-                <a href={link} key={id} target="_blank" rel="noopener noreferrer">
+                <a href={link} key={id} target="_blank" rel="noopener noreferrer" >
                     <Image src={src} alt={name} width={30} height={30} />
                 </a>
             ))}
--- src/components/Layout/Footer/FooterColumn.tsx
+++ src/components/Layout/Footer/FooterColumn.tsx
@@ -1,11 +1,11 @@
 export default function FooterColumn({ title, links }: { title: string; links: string[] }) {
     return (
         <div>
-            <h6 className="mb-4 font-semibold uppercase">{title}</h6>
+            <h6 className="mb-4 font-neuBold uppercase">{title}</h6>
             <ul>
                 {links.map((link, index) => (
-                    <li key={index} className="mb-2">
-                        <a href="#" className="text-neutral-600 dark:text-neutral-200 hover:underline">
+                    <li key={index} className="mb-5 font-neuBook tracking-wider">
+                        <a href="#" className="text-[#444444] hover:underline cursor-pointer text-[14px]">
                             {link}
                         </a>
                     </li>
