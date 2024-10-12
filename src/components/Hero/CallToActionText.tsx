dece8575d4d993a846abc2c51de3e681cfb8facd
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-12 19:25:49 +0700

    refactor : refactor the code structure to be cleaner

--- src/components/Hero/CallToActionText.tsx
+++ src/components/Hero/CallToActionText.tsx
@@ -1,16 +1,17 @@
 import ButtonCustom from "../Button";
 import SosialMediaIcon from "../Icon";
 
-
 export function CallToActionText() {
-    return (
-        <div className="w-full">
-            <div className="py-10 w-full gap-5 lg:flex">
-                <ButtonCustom src="/svg/User.svg" className="w-full lg:w-auto ">More about me</ButtonCustom>
-                <div className="gap-5 flex items-center justify-center py-6 lg:py-0 lg:">
-                    <SosialMediaIcon />
-                </div>
-            </div>
+  return (
+    <div className="w-full">
+      <div className="w-full gap-5 py-10 lg:flex">
+        <ButtonCustom src="/svg/User.svg" className="w-full lg:w-auto ">
+          More about me
+        </ButtonCustom>
+        <div className="lg: flex items-center justify-center gap-5 py-6 lg:py-0">
+          <SosialMediaIcon />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
