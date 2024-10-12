e4acfff3e73359f0e5f85d5ecab8cd331740696a
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-12 20:41:49 +0700

    refactor : call component into file

---
+++ src/views/Projects/index.tsx
@@ -0,0 +1,10 @@
+import Heading from "@/components/Layout/Heading";
+import { CardProject } from "@/components/Projects/Card";
+
+export default function ProjectSection() {
+  return (
+    <Heading heading="Projects">
+      <CardProject />
+    </Heading>
+  );
+}
