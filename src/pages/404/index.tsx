78cb2e9ba8c635514f577fc2d880be8b653c5309
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-12 20:42:33 +0700

    feat : initial component

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="w-full">
+      <h1>Not Found Page</h1>
+    </div>
+  );
+}
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="h-[100vh] w-full">
+      <h1>Contact</h1>
+    </div>
+  );
+}
