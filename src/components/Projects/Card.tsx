5ec42c02524138fb02b3577a0004d0828c51a9ce
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-12 19:46:17 +0700

    feat : create component card for project page

--- src/components/Projects/Card.tsx
+++ src/components/Projects/Card.tsx
@@ -1,22 +1,38 @@
 import Image from "next/image";
-import { PROJECTS } from "@/data/projects.ts";
+import { LIST_PROJECTS } from "@/data/projects.ts";
 
 export function CardProject() {
-    return (
-        <div className="w-full font-neuBook tracking-wider">
-            {PROJECTS.map((item) => (
-                <div className="flex gap-3 lg:py-4 py-3" key={item.id}>
-                    <Image src={item.image} alt={item.title} width={100} height={67} className="rounded-lg" />
-                    <div className="items-center ">
-                        <div className="flex gap-2">
-                            <h1 className="text-[14px] md:text-[18px] font-neuBold">{item.title}</h1>
-                            <p>{item.star}</p>
-                        </div>
-                        <p className="text-[14px] md:text-[16px]">{item.desc.slice(0, 50).concat("...")}</p>
-                        <p className="text-[14px] md:text-[16px] text-[#444444] font-neuThin">{item.date}</p>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="w-full font-neuBook tracking-wider">
+      {LIST_PROJECTS.map((item) => (
+        <div
+          key={item.id}
+          style={{ "--color": item.color } as React.CSSProperties}
+          className={`flex gap-3 rounded-lg py-3  pl-4 lg:py-4 hover:bg-[var(--color)] group transition-all duration-75`}
+        >
+          <Image
+            src={item.image}
+            alt={item.title}
+            width={100}
+            height={67}
+            className="rounded-lg group-hover:scale-105 transition-all duration-75 will-change-transform"
+          />
+          <div className="items-center">
+            <div className="flex gap-2">
+              <h1 className="font-neuBold text-[14px] md:text-[18px]">
+                {item.title}
+              </h1>
+              <p>{item.star}</p>
+            </div>
+            <p className="text-[14px] md:text-[16px]">
+              {item.desc.slice(0, 50).concat("...")}
+            </p>
+            <p className="font-neuThin text-[14px] text-[#444444] md:text-[16px]">
+              {item.date}
+            </p>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
