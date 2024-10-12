45ba3df0f2a2d0c9b49501ace723d624747591c8
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-12 19:47:11 +0700

    feat : add dummy data (Blog, iconsList, listMySkill and project)

--- src/data/iconList.ts
+++ src/data/iconList.ts
@@ -1,32 +1,38 @@
 export const ICON_LIST = [
-    {
-        id: 1,
-        name: "youtube",
-        src: "/svg/Youtube.svg",
-        link: "https://www.youtube.com/@JOJO-dc3wm",
-    },
-    {
-        id: 2,
-        name: "instagram",
-        src: "/svg/Instagram.svg",
-        link: "https://www.instagram.com/madds.ar",
-    },
-    {
-        id: 3,
-        name: "Facebook",
-        src: "/svg/Facebook.svg",
-        link: "https://www.facebook.com/madds.ar",
-    },
-    {
-        id: 4,
-        name: "Gmail",
-        src: "/svg/Gmail.svg",
-        link: "https://mail.google.com/mail/u/0/#inbox",
-    },
-    {
-        id: 5,
-        name: "Github",
-        src: "/svg/Github.svg",
-        link: "https://github.com/AhmadJanuarr",
-    }
+  {
+    id: 1,
+    name: "youtube",
+    src: "/svg/Youtube.svg",
+    link: "https://www.youtube.com/@JOJO-dc3wm",
+  },
+  {
+    id: 2,
+    name: "instagram",
+    src: "/svg/Instagram.svg",
+    link: "https://www.instagram.com/madds.ar",
+  },
+  {
+    id: 3,
+    name: "Facebook",
+    src: "/svg/Facebook.svg",
+    link: "https://www.facebook.com/madds.ar",
+  },
+  {
+    id: 4,
+    name: "Gmail",
+    src: "/svg/Gmail.svg",
+    link: "https://mail.google.com/mail/u/0/#inbox",
+  },
+  {
+    id: 5,
+    name: "Github",
+    src: "/svg/Github.svg",
+    link: "https://github.com/AhmadJanuarr",
+  },
+  {
+    id: 6,
+    name: "Linkedin",
+    src: "/svg/Linkedin.svg",
+    link: "https://www.linkedin.com/in/ahmad-januar-a96515221/",
+  }
 ];
