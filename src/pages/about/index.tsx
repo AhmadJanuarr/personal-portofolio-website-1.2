8f9c9584ea4f6596710c57f2ec7314a686dad3b7
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-15 02:11:32 +0700

    feat: Implement server-side rendering for homepage

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,13 +2,26 @@ import { Container } from "..";
 import Introduce from "@/views/About/about";
 import Heading from "@/components/Layout/Heading";
 import Skill from "@/views/About/skill";
+import Experience from "@/views/About/experience";
+import { AbilityType } from "@/types/ability.type";
 
-export default function About() {
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/skills");
+  const response = await res.json();
+  return {
+    props: {
+      ability: response.data,
+    },
+  };
+}
+
+export default function About({ ability }: { ability: AbilityType[] }) {
   return (
     <Container>
       <Heading heading="About">
         <Introduce />
-        <Skill />
+        <Skill ability={ability} />
+        <Experience />
       </Heading>
     </Container>
   );
