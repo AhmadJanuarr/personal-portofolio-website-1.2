1235eee71ec676060af69d5c2ef76bf353c094ce
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-15 02:47:08 +0700

    feat : install firestore and config for firebase storage with remotepatterns and enable and dangerouslyAllowSVG

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
