04599d6032abb7c804a6c274f9bfead88e03954f
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-16 14:36:46 +0700

    refactor: clean up and improve readability of CardProject components

--- src/components/Projects/Card.tsx
+++ src/components/Projects/Card.tsx
@@ -1,43 +1,67 @@
 import Image from "next/image";
-import { LIST_PROJECTS } from "@/data/projects.ts";
-import StartContainer from "./Star";
+import StarContainer from "./Star";
+import { useProjectStore } from "@/stores/projectStore";
+import { useEffect } from "react";
+import { ProjectsType } from "../../types/projets.type";
 
 export default function CardProject() {
+  const { projects, fetchProjects, isLoading } = useProjectStore();
+
+  useEffect(() => {
+    fetchProjects();
+  }, [fetchProjects]);
+
   return (
     <div className="w-full font-neuBook tracking-wider">
-      {LIST_PROJECTS.map((item) => (
+      {isLoading && <p className="text-[14px]">Loading...</p>}
+
+      {projects.map(({ id, color, image, title, star, description, date, technologies }: ProjectsType) => (
         <div
-          key={item.id}
-          style={{ "--color": item.color } as React.CSSProperties}
-          className={`flex gap-3 rounded-lg py-3  pl-4 lg:py-4 hover:bg-[var(--color)] group transition-all duration-75 `}
+          key={id}
+          style={{ "--color": color } as React.CSSProperties}
+          className="group flex gap-3 rounded-lg py-3 pl-4 lg:py-4 transition-all duration-75 hover:bg-[var(--color)]"
         >
           <div className="relative">
             <Image
-              src={item.image}
-              alt={item.title}
+              src={image}
+              alt={title}
               width={100}
               height={100}
               sizes="(max-width: 768px) 100px, (max-width: 1390px) 130px, 130px"
-              className="object-cover rounded-lg group-hover:scale-105 transition-all duration-75 will-change-transform "
+              className="rounded-lg object-cover transition-all duration-75 group-hover:scale-105"
             />
           </div>
-          <div className="items-center">
-            <div className="flex gap-2">
+
+          <div className="w-full">
+            <div className="flex gap-2 items-center">
               <h1 className="font-neuBold text-[14px] md:text-[18px]">
-                {item.title}
+                {title}
               </h1>
-              <StartContainer>{item.star}</StartContainer>
+              <StarContainer>{star}</StarContainer>
             </div>
+
             <p className="text-[14px] md:text-[16px]">
-              {item.desc.slice(0, 50).concat("...")}
+              {description.slice(0, 50)}...
             </p>
-            <p className="font-neuThin text-[14px] text-[#444444] md:text-[16px]">
-              {item.date}
+
+            <p className="font-neuThin text-[14px] text-[#444444] md:text-[14px]">
+              {date}
             </p>
+
+            {technologies && (
+              <div className="hidden lg:flex gap-2 w-full">
+                {technologies.map((tech) => (
+                  <div className="bg-[#F4F6FF] rounded-lg border items-center" key={tech}>
+                    <button className="font-neuBook text-[12px] text-[#444444] md:text-[12px] px-3">
+                      {tech}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
-      ))
-      }
-    </div >
+      ))}
+    </div>
   );
 }
--- src/components/Projects/Star.tsx
+++ src/components/Projects/Star.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-export default function StartContainer({ children }: { children: React.ReactNode }) {
+export default function StarContainer({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex items-center gap-2 justify-center border rounded-lg px-2 bg-[#f6f8Fa]">
             <Image src="/svg/Star.svg" alt="star" width={15} height={15} />
-            <span className="text-[12px] md:text-[14px]">{children}</span>
+            <span className="text-[12px] ">{children}</span>
         </div>
     )
 }
