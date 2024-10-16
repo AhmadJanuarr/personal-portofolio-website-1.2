eb45e23c5fe7d78c02e8269779468479f303c15c
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-16 14:22:25 +0700

    add : data type to projects

---
+++ src/types/projets.type.ts
@@ -0,0 +1,11 @@
+export interface ProjectsType {
+    id: string;
+    title: string;
+    date: string;
+    color: string;
+    description: string;
+    star: number;
+    image: string;
+    status?: string
+    technologies?: string[]
+}
