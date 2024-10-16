702c88b50b947dfed447aba304e9f995e02dd50d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-16 19:27:14 +0700

    add(file) : data dummy for my experience

---
+++ src/data/experience.ts
@@ -0,0 +1,19 @@
+export const LIST_EXPERIENCE = [
+    {
+        id: 1,
+        title: "Translation dan Copywriting",
+        company: "PT Sema Information Technology Services",
+        type: "Freelance",
+        date: "Jan 2024 - Current",
+        lenght: "10 month",
+        logo: "/img/sema.jpg"
+
+    }, {
+        id: 2,
+        title: "Secretariat",
+        company: "Dinas Perhubungan Kota Bandar Lampung",
+        type: "Student Intern",
+        date: "Nov 2022 - Jan 2023",
+        lenght: "3 month",
+        logo: "/img/dishub.png"
+    }]
