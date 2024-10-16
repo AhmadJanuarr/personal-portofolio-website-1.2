1518eddaa348e036d085805b3c5328be092711f5
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-16 14:31:57 +0700

    feat(zustand) : zustand store configuration to store project data

---
+++ src/stores/projectStore.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { ProjectsType } from "../types/projets.type";
+
+type ProjectState = {
+    projects: ProjectsType[];
+    fetchProjects: () => Promise<void>;
+    isLoading: boolean
+}
+
+export const useProjectStore = create<ProjectState>((set) => ({
+    projects: [],
+    isLoading: false,
+    fetchProjects: async () => {
+        set({ isLoading: true });
+        try {
+            const res = await fetch("http://localhost:3000/api/projects");
+            const response = await res.json();
+            set({ projects: response.data });
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+            set({ projects: [] });
+        } finally {
+            set({ isLoading: false });
+        }
+    }
+})) 
