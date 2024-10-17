74a6555c24bb6fab2459bc390ba5eb794a520910
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-17 20:17:42 +0700

    style: format all components with Prettier and Tailwind

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,28 +1,28 @@
-import { Container } from "..";
-import { AbilityType } from "@/types/ability.type";
-import Introduce from "@/views/About/about";
-import Heading from "@/components/Layout/Heading";
-import Skill from "@/views/About/skill";
-import Experience from "@/views/About/experience";
+import { Container } from ".."
+import { AbilityType } from "@/types/ability.type"
+import Introduce from "@/views/About/about"
+import Heading from "@/components/Layout/Heading"
+import Skill from "@/views/About/skill"
+import Experience from "@/views/About/experience"
 
 export async function getServerSideProps() {
-  const res = await fetch("http://localhost:3000/api/ability");
-  const response = await res.json();
-  return {
-    props: {
-      ability: response.data,
-    },
-  };
+    const res = await fetch("http://localhost:3000/api/ability")
+    const response = await res.json()
+    return {
+        props: {
+            ability: response.data,
+        },
+    }
 }
 
 export default function About({ ability }: { ability: AbilityType[] }) {
-  return (
-    <Container>
-      <Heading heading="About">
-        <Introduce />
-        <Skill ability={ability} />
-        <Experience />
-      </Heading>
-    </Container>
-  );
+    return (
+        <Container>
+            <Heading heading="About">
+                <Introduce />
+                <Skill ability={ability} />
+                <Experience />
+            </Heading>
+        </Container>
+    )
 }
