2c2604dce924f922d31914f542d3bd85b786f848
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-17 20:18:48 +0700

    refactor : updating the writing placement to be cleaner

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,21 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { retriveData } from "@/lib/firebase/service";
-import { ProjectsType } from "@/types/projets.type";
+import { NextApiRequest, NextApiResponse } from "next"
+import { retriveData } from "@/lib/firebase/service"
+import { ProjectsType } from "@/types/projets.type"
 
 type Data = {
-    status: boolean;
-    statusCode: number;
-    data: ProjectsType[];
-};
+    status: boolean
+    statusCode: number
+    data: ProjectsType[]
+}
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
 ) {
     try {
-        const data = await retriveData("projects");
-        res.status(200).json({ status: true, statusCode: 200, data: data.projects });
+        const data = await retriveData("projects")
+        res.status(200).json({
+            status: true,
+            statusCode: 200,
+            data: data.projects,
+        })
     } catch (error) {
-        console.error("Error fetching data: ", error);
-        res.status(500).json({ status: false, statusCode: 500, data: [] });
+        console.error("Error fetching data: ", error)
+        res.status(500).json({ status: false, statusCode: 500, data: [] })
     }
 }
