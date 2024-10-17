f8339b8c2bd4edf2c80a38ece4c5ba859506c70d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-17 20:37:08 +0700

    fix : fix the writing to keep it clean

--- src/stores/projectStore.ts
+++ src/stores/projectStore.ts
@@ -1,9 +1,9 @@
-import { create } from "zustand";
-import { ProjectsType } from "../types/projets.type";
+import { create } from "zustand"
+import { ProjectsType } from "../types/projets.type"
 
 type ProjectState = {
-    projects: ProjectsType[];
-    fetchProjects: () => Promise<void>;
+    projects: ProjectsType[]
+    fetchProjects: () => Promise<void>
     isLoading: boolean
 }
 
@@ -11,16 +11,16 @@ export const useProjectStore = create<ProjectState>((set) => ({
     projects: [],
     isLoading: false,
     fetchProjects: async () => {
-        set({ isLoading: true });
+        set({ isLoading: true })
         try {
-            const res = await fetch("http://localhost:3000/api/projects");
-            const response = await res.json();
-            set({ projects: response.data });
+            const res = await fetch("http://localhost:3000/api/projects")
+            const response = await res.json()
+            set({ projects: response.data })
         } catch (error) {
-            console.error("Error fetching data: ", error);
-            set({ projects: [] });
+            console.error("Error fetching data: ", error)
+            set({ projects: [] })
         } finally {
-            set({ isLoading: false });
+            set({ isLoading: false })
         }
-    }
-})) 
+    },
+}))
