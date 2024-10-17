3f30ca0fb2a2a7bbb6a38c4100317d94f8ab2dd0
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-17 20:16:35 +0700

    refactor : updating components section Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,30 @@
-import Hero from "@/views/Home/Hero";
-import SectionHeader from "@/components/Sections/SectionHeader";
-import CardProject from "@/components/Projects/Card";
-import BlogItem from "@/components/Blog/Item";
+import Hero from "@/views/Home/Hero"
+import SectionHeader from "@/components/Sections/SectionHeader"
+import CardProject from "@/components/Projects/Card"
+import BlogItem from "@/components/Blog/Item"
 
 export function Container({ children }: { children: React.ReactNode }) {
-  return <div className="pt-28 md:pt-32">{children}</div>;
+    return <div className="pt-28 md:pt-32">{children}</div>
 }
 
 export default function Home() {
-  return (
-    <Container>
-      <Hero />
-      <SectionHeader heading="Featured Projects" href="/projects">
-        <CardProject />
-      </SectionHeader>
-      <SectionHeader heading="From the blog" href="/blog">
-        <BlogItem />
-      </SectionHeader>
-    </Container>
-  );
+    return (
+        <Container>
+            <Hero />
+            <SectionHeader
+                heading="Featured Projects"
+                href="/projects"
+                showButton={true}
+            >
+                <CardProject />
+            </SectionHeader>
+            <SectionHeader
+                heading="From the blog"
+                href="/blog"
+                showButton={true}
+            >
+                <BlogItem />
+            </SectionHeader>
+        </Container>
+    )
 }
