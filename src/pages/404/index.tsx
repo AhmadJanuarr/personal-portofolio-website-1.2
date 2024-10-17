a5d27fbf925a5c51954b9e340dfdda72138fe09b
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-17 20:17:13 +0700

    feat : add components notfoundpage

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image"
+import { Container } from ".."
+import ButtonCustom from "../../components/Button"
+
 export default function NotFoundPage() {
-  return (
-    <div className="w-full">
-      <h1>Not Found Page</h1>
-    </div>
-  );
+    return (
+        <Container>
+            <div className="flex w-full flex-col items-center  gap-3 pb-32 pt-10 tracking-wider">
+                <Image
+                    src="/img/NotFound.png"
+                    width={386}
+                    height={280}
+                    alt="not found"
+                />
+                <h4 className="font-neuBold text-[24px]">
+                    😕 Oops! Page Not Found
+                </h4>
+                <p className="pb-5 font-neuMedium text-[16px]">
+                    Looks like the page you&apos;re looking for doesn&apos;t
+                    exist. It might have been moved or deleted.
+                </p>
+                <ButtonCustom href="/" src="/svg/Arrow.svg">
+                    Back to Home
+                </ButtonCustom>
+            </div>
+        </Container>
+    )
 }
