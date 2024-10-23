94aac08816318b7decfe3fccb00bba827b5e0a66
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-23 17:41:15 +0700

    refactor : remove blank spaces

--- src/pages/api/[[...projects]].ts
+++ src/pages/api/[[...projects]].ts
@@ -11,12 +11,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
 ) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
     try {
-        const projectTitle = req.query.projects ? req.query.projects[1] : null;
+        const projectTitle = req.query.projects ? req.query.projects[1] : null
         console.log(projectTitle)
         if (projectTitle) {
             const data = await retriveDataByName("projects", projectTitle)
@@ -24,27 +23,24 @@ export default async function handler(
                 res.status(200).json({
                     status: true,
                     statusCode: 200,
-                    data: data as ProjectsType[]
+                    data: data as ProjectsType[],
                 })
             } else {
                 res.status(404).json({
                     status: false,
                     statusCode: 404,
-                    data: []
+                    data: [],
                 })
             }
-
-        }
-        else {
+        } else {
             const data = await retriveData("projects")
             return res.status(200).json({
                 status: true,
                 statusCode: 200,
-                data: data.projects
+                data: data.projects,
             })
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.error("Error fetching data: ", error)
         res.status(500).json({ status: false, statusCode: 500, data: null })
     }
