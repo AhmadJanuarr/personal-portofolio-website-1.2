9002c57473c01da481c78bcdc70ee15375f99ecc
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-26 16:12:12 +0700

    feat : add slice feature to data project and change styling naming in classname

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,8 +5,10 @@ import Link from "next/link"
 import SkeletonCard from "../Skeleton/SkeletonCard"
 import { ProjectsType } from "@/types/projects.type"
 import { fetching } from "@/lib/swr/fetch"
+import { useRouter } from "next/router"
 
 export default function CardProject() {
+    const { pathname } = useRouter()
     const { data, isLoading } = useSWR(
         `${process.env.NEXT_PUBLIC_API_URL}/api/projects`,
         fetching,
@@ -17,55 +19,61 @@ export default function CardProject() {
         <div className="w-full font-neuBook tracking-wider">
             {!isLoading ? (
                 <>
-                    {projectData.map(
-                        ({
-                            id,
-                            color,
-                            image,
-                            title,
-                            star,
-                            description,
-                            date,
-                        }: ProjectsType) => (
-                            <Link
-                                href={`projects/${title}`}
-                                key={id}
-                                style={
-                                    { "--color": color } as React.CSSProperties
-                                }
-                                className="group flex cursor-pointer gap-3 rounded-lg py-3 pl-4 transition-all duration-75 hover:bg-[var(--color)] lg:py-4"
-                            >
-                                <div className="relative">
-                                    <Image
-                                        src={image}
-                                        alt={title}
-                                        width={100}
-                                        height={100}
-                                        sizes="(max-width: 768px) 100px, (max-width: 1390px) 130px, 130px"
-                                        className="rounded-lg object-cover transition-all duration-75 group-hover:scale-105"
-                                    />
-                                </div>
-                                <div className="w-full">
-                                    <div className="flex items-center gap-2">
-                                        <h1 className="font-neuBold text-[14px] md:text-[18px]">
-                                            {title}
-                                        </h1>
-                                        <StarContainer>{star}</StarContainer>
+                    {projectData
+                        .slice(0, pathname === "/" ? 4 : 10)
+                        .map(
+                            ({
+                                id,
+                                color,
+                                image,
+                                title,
+                                star,
+                                description,
+                                date,
+                            }: ProjectsType) => (
+                                <Link
+                                    href={`projects/${title}`}
+                                    key={id}
+                                    style={
+                                        {
+                                            "--color": color,
+                                        } as React.CSSProperties
+                                    }
+                                    className="group flex cursor-pointer gap-3 rounded-lg py-3 pl-4 transition-all duration-75 hover:bg-[var(--color)] lg:py-4"
+                                >
+                                    <div className="relative">
+                                        <Image
+                                            src={image}
+                                            alt={title}
+                                            width={100}
+                                            height={100}
+                                            sizes="(max-width: 768px) 100px, (max-width: 1390px) 130px, 130px"
+                                            className="rounded-lg object-cover transition-all duration-75 group-hover:scale-105"
+                                        />
                                     </div>
-                                    <p className="mb-1 text-[12px] md:text-[16px]">
-                                        {description.slice(
-                                            0,
-                                            screen.width < 660 ? 50 : 100,
-                                        )}
-                                        ...
-                                    </p>
-                                    <p className="font-neuThin text-[11px] text-[#444444] md:text-[14px]">
-                                        {date}
-                                    </p>
-                                </div>
-                            </Link>
-                        ),
-                    )}
+                                    <div className="w-full">
+                                        <div className="flex items-center gap-2">
+                                            <h1 className="font-neuBold text-[14px] md:text-[18px]">
+                                                {title}
+                                            </h1>
+                                            <StarContainer>
+                                                {star}
+                                            </StarContainer>
+                                        </div>
+                                        <p className="paragraph mb-1 font-neuBook">
+                                            {description.slice(
+                                                0,
+                                                screen.width < 660 ? 50 : 100,
+                                            )}
+                                            ...
+                                        </p>
+                                        <p className="font-neubook text-[11px] text-[#444444] md:text-[14px]">
+                                            {date}
+                                        </p>
+                                    </div>
+                                </Link>
+                            ),
+                        )}
                 </>
             ) : (
                 <SkeletonCard lenght={5} />
