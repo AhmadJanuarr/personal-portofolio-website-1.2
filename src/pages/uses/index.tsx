bb450afdbfdff7dd2e6c65788d51e77529d375ad
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-27 14:52:10 +0700

    feat : added a navmenu feature for content

--- src/pages/uses/index.tsx
+++ src/pages/uses/index.tsx
@@ -5,68 +5,66 @@ import BrowserSetup from "@/views/Uses/BrowserSetup"
 import useSWR from "swr"
 import { Container } from ".."
 import { fetching } from "@/lib/swr/fetch"
-import { MouseEvent, useEffect, useState } from "react"
-import { SoftwareType } from "@/types/software.type"
+import { MouseEvent, useState } from "react"
+
+function NavMenu({
+  onClick,
+  value,
+  children,
+  className,
+}: {
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void
+  value: string
+  children: React.ReactNode
+  className?: string
+}) {
+  return (
+    <li>
+      <button onClick={onClick} value={value} className={`font-neuMedium text-[14px] md:text-[16px] ${className}`}>
+        {children}
+      </button>
+    </li>
+  )
+}
 
 export default function Uses() {
   const [current, setCurrent] = useState<string>("all")
-  const [filteredData, setFilteredData] = useState([])
   const { data, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/softwares`, fetching)
   const softwareData = data?.data || []
 
-  console.log(softwareData)
   const handleBtn = (e: MouseEvent<HTMLButtonElement>) => {
     const value = e.currentTarget.value
     setCurrent(value)
   }
-  useEffect(() => {
-    if (current === "all") {
-      setFilteredData(softwareData)
-    } else if (current === "software") {
-      setFilteredData(softwareData.filter((item: SoftwareType) => item.name === "software"))
-    } else if (current === "coding") {
-      setFilteredData(softwareData.filter((item: SoftwareType) => item.name === "coding"))
-    } else if (current === "browser") {
-      setFilteredData(softwareData.filter((item: SoftwareType) => item.name === "browser"))
-    }
-  }, [current, softwareData])
-
+  const NavActive = "font-neuBold text-secondary"
   return (
     <Container>
       <Heading heading="What do I use?">
-        <div className="flex w-full border-b-2">
-          <ul className="flex gap-5 py-5">
-            <li>
-              <button onClick={handleBtn} value="all">
-                All
-              </button>
-            </li>
-            <li>
-              <button onClick={handleBtn} value="software">
-                Software
-              </button>
-            </li>
-            <li>
-              <button onClick={handleBtn} value="coding">
-                Coding
-              </button>
-            </li>
-            <li>
-              <button onClick={handleBtn} value="browser">
-                Browser
-              </button>
-            </li>
+        <div className="my-8 flex w-full border-b-2">
+          <ul className="flex gap-8 py-3 tracking-wider md:py-5">
+            <NavMenu onClick={handleBtn} value="all" className={`${current === "all" && NavActive}`}>
+              All
+            </NavMenu>
+            <NavMenu onClick={handleBtn} value="software" className={`${current === "software" && NavActive}`}>
+              Software
+            </NavMenu>
+            <NavMenu onClick={handleBtn} value="coding" className={`${current === "coding" && NavActive}`}>
+              Coding
+            </NavMenu>
+            <NavMenu onClick={handleBtn} value="browser" className={`${current === "browser" && NavActive}`}>
+              Browser
+            </NavMenu>
           </ul>
         </div>
         <div className="w-full">
           {current === "all" && (
             <>
-              <SoftwareShowcase data={filteredData} isLoading={isLoading} />
+              <SoftwareShowcase data={softwareData} isLoading={isLoading} />
               <EditorSetup />
               <BrowserSetup />
             </>
           )}
-          {current === "software" && <SoftwareShowcase data={filteredData} isLoading={isLoading} />}
+          {current === "software" && <SoftwareShowcase data={softwareData} isLoading={isLoading} />}
           {current === "coding" && <EditorSetup />}
           {current === "browser" && <BrowserSetup />}
         </div>
