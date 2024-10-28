8a594d58a85fbac6c5c2569cf8e5b359529226b1
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-10-28 09:11:23 +0700

    refactor : handle if data is not there and refactor code

--- src/views/Uses/SoftwareShowcase.tsx
+++ src/views/Uses/SoftwareShowcase.tsx
@@ -5,21 +5,25 @@ import SkeletonIconBox from "@/components/Skeleton/SkeletonIconBox"
 import { SoftwareType } from "@/types/software.type"
 
 export default function SoftwareShowcase({ data, isLoading }: { data: SoftwareType[]; isLoading: boolean }) {
-    return (
-        <div className="w-full">
-            <SectionHeader heading="My Software" showButton={false}>
-                <div className="flex w-full flex-wrap gap-3">
-                    {!isLoading ? (
-                        <React.Fragment>
-                            {data.map(({ id, name, url, icon, color }) => (
-                                <IconBox key={id} url={url || "#"} icon={icon} name={name} color={color} />
-                            ))}
-                        </React.Fragment>
-                    ) : (
-                        <SkeletonIconBox />
-                    )}
-                </div>
-            </SectionHeader>
+  return (
+    <div className="w-full">
+      <SectionHeader heading="My Software" showButton={false}>
+        <div className="flex w-full flex-wrap gap-3">
+          {!isLoading ? (
+            <React.Fragment>
+              {!data ? (
+                <p>No data</p>
+              ) : (
+                data.map(({ id, name, url, icon, color }) => (
+                  <IconBox key={id} url={url || "#"} icon={icon} name={name} color={color} />
+                ))
+              )}
+            </React.Fragment>
+          ) : (
+            <SkeletonIconBox />
+          )}
         </div>
-    )
+      </SectionHeader>
+    </div>
+  )
 }
